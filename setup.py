646d4dc8710109632d75da7da5fef1e864652e03
Peter Newstein <peternewstein@gmail.com>
2020-11-11 19:00:13 -0500

    restructured into a python package

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="sha256_check",
+    author="federicotorrielli",
+    # author_email="github.com/federicotorrielli",
+    description="quick SHA256 hash check",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pnewstein/git_tools",
+    packages=["sha256_check"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.4',
+    install_requires=["colorama>=0.4.3"]
+)
